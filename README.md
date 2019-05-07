bc9dc1bdb8a18dbdfe337c3763fd864d644975a6
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2019-05-07 18:36:47 +0200

    Version 0.0.1

---
+++ README.md
@@ -0,0 +1,7 @@
+Scratch3 to HTML  
+================  
+  
+Application: https://sheeptester.github.io/words-go-here/scratch3-htmlifier/  
+Code source: https://sheeptester.github.io/all/  
+  
+
---
+++ scratch2html/sheep3/sheep3.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+  var homeButton = document.createElement("sheep-btn");
+  homeButton.setAttribute('tabindex', 0);
+  homeButton.setAttribute('aria-label', 'Go to SheepTester directory');
+  homeButton.addEventListener('click', function(e) {
+    document.body.classList.add('sheep-blockscreen');
+    homeButton.addEventListener('transitionend', function(e) {
+      window.location = '/?from=sheep3';
+    });
+  });
+  homeButton.addEventListener('keydown', function(e) {
+    if (e.keyCode === 13) homeButton.click();
+  });
+  document.body.appendChild(homeButton);
+}, {once: true});
